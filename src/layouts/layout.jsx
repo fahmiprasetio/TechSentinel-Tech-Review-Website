9fc51a3b1b123a894544325373c9365eade75f96
fahmiprasetio <nfahmi086@gmail.com>
2025-04-28 13:57:14 +0700

    Update projek : Pembuatan awal homepage, penambahan background untuk semua halaman

---
+++ src/layouts/layout.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import LayoutNavbar from './layoutnavbar';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="relative flex flex-col min-h-screen overflow-hidden">
+      {/* Background Fixed */}
+      <div
+        className="fixed top-0 left-0 w-full h-full -z-10"
+        style={{
+          backgroundImage: "url('/Background-3.png')",
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+        }}
+      ></div>
+
+      {/* Content */}
+      <LayoutNavbar />
+      <main className="mt-16 flex-grow">
+        {children}
+      </main>
+
+      {/* Footer */}
+      <footer className="w-full bg-gray-900 h-30 text-white py-4">
+        <div className="text-center">
+          <p>&copy; 2025 Tech Commitment. All Rights Reserved.</p>
+          <p>Contact: info@techcommitment.com</p>
+        </div>
+      </footer>
+      
+    </div>
+  );
+};
+
+export default Layout;
