a122dc1f14282f00bbd3ac6389594b9f311c4473
Agata Mulia R <muliaramadhan3@gmail.com>
2025-06-06 20:48:53 +0700

    navbar

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaEnvelope, FaLock } from "react-icons/fa";
-import axios from "axios";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
